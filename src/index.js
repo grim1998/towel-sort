2f7aca3a7225fd700ec5e0bfd98d216630140a2e
grim1998 <gmirmaksim8@gmail.com>
2023-01-23 11:15:29 +0300

    add:towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length === 0) {
+    return [];
+  };
+  
+  const reversedArr = matrix.map((item, index) => {
+    if (index % 2 === 0) {
+      return item;
+    } else {
+      return item.reverse();
+    };
+  });  
+  const towel = reversedArr.toString().split(',').map(item => +(item));
+  return towel;
 }
